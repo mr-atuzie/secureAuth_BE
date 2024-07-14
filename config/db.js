df4d91f7d81a8cd94d3156677b88da38e6b02fc3
mr-atuzie <user email>
2024-07-14 17:01:16 +0100

    --bug--

--- config/db.js
+++ config/db.js
@@ -5,9 +5,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB);
 
-    console.log(
-      `Database Connected: ${conn.connection.host}`.cyan.underline.bold
-    );
+    console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
   }
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const colors = require("colors");
 const connectDB = require("./config/db");
 const userRoute = require("./routes/user");
 const { errorHandler, notFound } = require("./middleware/errorMiddleware");
