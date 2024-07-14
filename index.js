2b08fcf0fa4fcaa5ce08eabb84e8014b356a686f
mr-atuzie <user email>
2024-07-14 16:57:48 +0100

    --bug--

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://2fa-form.netlify.app"],
     credentials: true,
   })
 );
